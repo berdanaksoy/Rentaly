271bcf74ae3cd5a56631da4dbf164314d83f8a21
Berdan Aksoy <berdan0227@gmail.com>
2026-03-03 16:03:00 +0300

    Add project files.

---
+++ Rentaly.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rentaly.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T>
+    {
+        Task InsertAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task<List<T>> GetListAsync();
+        Task<T> GetByIdAsync(int id);
+    }
+}
---
+++ Rentaly.EntityLayer/Entities/Brand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rentaly.EntityLayer.Entities
+{
+    public class Brand
+    {
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
